37558c708527fe4fca8aaa418257fee2b6b3b5ef
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2023-04-23 20:08:20 +0530

    signup added with alert and form

--- src/components/UsersF/Signup.js
+++ src/components/UsersF/Signup.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './signup.css'
 import { useNavigate } from "react-router";
 export default function Signup(props) {
+
   const [credentials, setCredentials] = useState({ email: "", password: "",name:"",cpassword:""});
   let navigate = useNavigate();
   const onChange = (e) => {
@@ -14,7 +15,6 @@ export default function Signup(props) {
       props.showAlert("Password and confirm Password are different!",'danger');
     }
     else{
-
       const url = "API_URL";
       const response = await fetch(url, {
       method: 'POST',
@@ -25,11 +25,10 @@ export default function Signup(props) {
     });
     const json = await response.json()
     console.log(json);
-    if (json.success) {
+    if (json.success) { // if success
       //redirect
-      
       props.showAlert('Account Created Successfully','success');
-      localStorage.setItem('token', json.authToken);
+      localStorage.setItem('token', json.authToken);  //athentication token to be taken from jwt
       navigate('/');
     }
     else {
@@ -52,78 +51,38 @@ export default function Signup(props) {
               <div className="card-body p-md-5">
                 <div className="row justify-content-center">
                   <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                      Sign up
-                    </p>
-
+                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
                     <form className="mx-1 mx-md-4" onSubmit={handleSubmit}>
                       <div className="d-flex flex-row align-items-center mb-4 input">
                         <i className="fas fa-user fa-lg me-3 fa-fw" style={{marginBottom : '10%'}}></i>
                         <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="text"
-                            id="form3Example1c"
-                            className="form-control"
-                            onChange={onChange}
-                          />
-                          <label className="form-label" for="form3Example1c">
-                            Your Name
-                          </label>
+                          <input type="text" id="name" name='name' className="form-control" onChange={onChange} required  minLength={3}/>
+                          <label className="form-label" htmlFor="name">Your Name</label>
                         </div>
                       </div>
-
                       <div className="d-flex flex-row align-items-center mb-4">
                         <i className="fas fa-envelope fa-lg me-3 fa-fw" style={{marginBottom : '10%'}}></i>
                         <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="email"
-                            id="form3Example3c"
-                            className="form-control"
-                            onChange={onChange}
-                          />
-                          <label className="form-label" for="form3Example3c">
-                            Your Email
-                          </label>
+                          <input type="email" id="email" className="form-control" name="email" onChange={onChange} required/>
+                          <label className="form-label" htmlFor="email">Your Email </label>
                         </div>
                       </div>
-
                       <div className="d-flex flex-row align-items-center mb-4">
                         <i className="fas fa-lock fa-lg me-3 fa-fw" style={{marginBottom : '10%'}}></i>
                         <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            id="form3Example4c"
-                            className="form-control"
-                            onChange={onChange}
-                          />
-                          <label className="form-label" for="form3Example4c">
-                            Password
-                          </label>
+                        <input type="password" name='password' className="form-control" id="password" onChange={onChange} required minLength={5}/>
+                        <label htmlFor="password" className="form-label">Password</label>
                         </div>
                       </div>
-
                       <div className="d-flex flex-row align-items-center mb-4">
                         <i className="fas fa-key fa-lg me-3 fa-fw" style={{marginBottom : '10%'}}></i>
                         <div className="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            id="form3Example4cd"
-                            className="form-control"
-                            onChange={onChange}
-                          />
-                          <label className="form-label" for="form3Example4cd">
-                            Repeat your password
-                          </label>
+                        <input type="password" name='cpassword' className="form-control" id="cpassword" onChange={onChange} required minLength={5} />
+                        <label htmlFor="cpassword" className="form-label">Confirm Password</label>
                         </div>
                       </div>
-
                       <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button
-                          type="button"
-                          className="btn btn-primary btn-lg"
-                        >
-                          Register
-                        </button>
+                        <button type="Submit" className="btn btn-primary btn-lg"> Register </button>
                       </div>
                     </form>
                   </div>

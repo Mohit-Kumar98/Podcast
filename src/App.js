494bd8f7f3d71058c51cd46784391d743417f0a0
Mohit-Kumar98 <92840979+Mohit-Kumar98@users.noreply.github.com>
2023-04-23 05:40:07 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
-import { GlobalContext } from './context/GlobalState';
+
+
 // importing components
 import Header from './components/HeaderF/Header';
 import Footer from './components/FooterF/Footer';
 import List from './components/ListF/List';
 import Search from './components/SearchF/Search';
-import Podcast from './containers/Podcast';
-import Account from './containers/Account';
 
 // importing global provider
-
+import { GlobalProvider } from "./context/GlobalState";
 import PodCastSummary from "./components/PodCastSummary";
 
 function App() {
-  const context=useContext(GlobalContext);
-  const {input}=context;
   const [user, setUser] = useState(null);
 
   const handleLogin = (username, password) => {
@@ -30,22 +27,12 @@ function App() {
 
   return (
       <div>
-       <GlobalProvider>
-       {/* <Router> */}
-        {/* <Router> */}
+        <GlobalProvider>
         <Header user={user} onLogout={handleLogout} />
         <Search/>
         <List/>
         <List/>
         <List/>
-        <Routes>
-          {/* <Route exact path="/" element={ <ListenList />} /> */}
-          <Route path="/listened" element={<Listened />} />
-          <Route path="/podcast/:id" element={<Podcast />}>
-          </Route>
-          <Route path="/account" element={<Account user={user} onLogout={handleLogout} /> }>
-          </Route>
-          </Routes>
       </GlobalProvider>
       </div>
   );
